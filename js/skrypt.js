cbfc89ebfc31a53535806e45b7d81ff94985f0a3
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-02-08 09:04:29 +0100

    Add js, html

--- js/skrypt.js
+++ js/skrypt.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var paperButton = document.getElementById('paper-button');
-var rockButton= document.getElementById('rock-button');
+var rockButton = document.getElementById('rock-button');
 var scissorsButton = document.getElementById('scissors-button');
-var newgameButton= document.getElementById('newgame-button');
-var output= document.getElementById('output');
-var modalOne=document.getElementById('modal-one');
+var newgameButton = document.getElementById('newgame-button');
+var output = document.getElementById('output');
+var modalOne = document.getElementById('modal-one');
 
 var computerChoice;
 var playerChoice;
@@ -13,6 +13,14 @@ var computerCounter=0;
 var playerCounter=0; 
 var roundsOfWin;
 var results;
+var params = {
+  computerChoice: [1,2,3],
+  playerChoice:  [1,2,3],
+  computerCounter: 0,
+  playerCounter: 0,
+  roundsOfWin: 'Ile rund chciałbyś zagrać?',
+  progress: []
+}
 
 function playerMove() {
   computerChoice = Math.floor((Math.random() * 3) +1);
@@ -32,26 +40,38 @@ function playerMove() {
     output.innerHTML = 'Computer WON';
     computerCounter++;
   }
-  function results() = params.progress.push ({
-      numerRundy: [1,2,...,+roundsOfWin+],
-      ruchGracza: +playerChoice+,
-      ruchKomputera: +computerChoice+,
-      wynikRundy: '<br/>' + playerCounter + ':' + computerCounter,
+  params.progress.push ({
+    ruchGracza: playerChoice,
+    ruchKomputera: computerChoice,
+    wynikRundy: '<br/>' +playerCounter+ ':' +computerCounter
   })
-}();
+}
+                         
+newgameButton.addEventListener('click', function(){
+  roundsOfWin = window.prompt('Ile rund chciałbyś zagrać?');
+});
+
+var buttons = document.querySelectorAll('.player-move');
+
+for (var i=0; i<buttons.lenght; i++) {
+  buttons[i].addEventListener('click', function() {
+    playerChoice = buttons[i].getAttribute('data-move');
+    play();
+  });
+}
 
 function gameOver() {
-  console.log(roundsOfWin);
-  if ((playerCounter==roundsOfWin) || (computerCounter==roundsOfWin)){
-    modalOne.innerHTML = 'Game over, please press the new game button!';
-    var addTable = '';
-
-    for (var i=0; i<params.progress.length; i++) {
-    addTable += '<td class="numberround">' + params.progress[i].numerRundy +'</td>''<td class="playermove">' + params.progress[i].ruchGracza +'</td>''<td class="computermove">' + params.progress[i].ruchKomputera +'</td>''<td class="win">' + params.progress[i].wynikRundy +'</td>';
-    }
   
-    document.write(addTable);
+  if ((playerCounter==roundsOfWin) || (computerCounter==roundsOfWin)) {
+    modalOne.innerHTML = 'Game over, please press the new game button!';
   }
+    
+  var addTable = '';
+
+  for (var i=0; i<params.progress.length; i++) {
+    addTable += '<td class="numberround">' +(i+1)+'</td><td class="playermove">' + params.progress[i].ruchGracza +'</td><td class="computermove">' + params.progress[i].ruchKomputera +'</td><td class="win">' + params.progress[i].wynikRundy +'</td>';
+  }
+  document.querySelector('#table table').innerHTML=addTable;
 }
 
 var play = function() {
@@ -59,74 +79,3 @@ var play = function() {
   gameOver();
 }
 
-paperButton.addEventListener('click', function() {
-  playerChoice = 1;
-  play();
-});
-
-rockButton.addEventListener('click', function() {
-  playerChoice = 2;
-  play();
- }); 
-
-scissorsButton.addEventListener('click', function() {
-  playerChoice= 3;
-  play();
-});
-                                
-newgameButton.addEventListener('click', function(){
- 	roundsOfWin = window.prompt('Ile rund chciałbyś zagrać?');
-});
-
-var buttons = document.querySelectorAll('.player-move');
-
-for (var i=0; i<buttons.lenght; i++) {
-  function playerMove() {
-        buttons.getAttribute("data-move");
-  }
-}
-
-var params = {
-      params.computerChoice: ['1','2','3'],
-      params.playerChoice:  ['1','2','3'],
-      params.computerCounter: 0,
-      params.playerCounter: 0,
-      params.roundsOfWin: 'Ile rund chciałbyś zagrać?',
-      params.progress: [],
-};
-
-(function(){ 
-var showModal = function(event){
-  event.preventDefault();
-  document.querySelector('#modal-overlay').classList.add('show');
-};
-  
-var modalLinks = document.querySelectorAll('.show-modal');
-  
-for(var i = 0; i < modalLinks.length; i++){
-    modalLinks[i].addEventListener('click', showModal);
-}
-
-var hideModal = function(event){
-  event.preventDefault();
-  document.querySelector('#modal-overlay').classList.remove('show');
-  table.innerHTML='',
-  params.progress.length=0;
-};
-  
-var closeButtons = document.querySelectorAll('.modal .close');
-  
-for(var i = 0; i < closeButtons.length; i++){
-    closeButtons[i].addEventListener('click', hideModal);
-}
-  
-document.querySelector('#modal-overlay').addEventListener('click', hideModal); 
-  
-var modals = document.querySelectorAll('.modal');
-  
-for(var i = 0; i < modals.length; i++){
-  modals[i].addEventListener('click', function(event){
-  event.stopPropagation();
-    });
-  }
-})()
